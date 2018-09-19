69eacc8f31d8ac828cae8d3fe9d9706177e6b265
Linas Jasinauskas <8286924+linasjasinauskas@users.noreply.github.com>
2018-09-19 17:23:33 +0300

    Fixed render months typing

--- types/DayPicker.d.ts
+++ types/DayPicker.d.ts
@@ -23,5 +23,5 @@ export default class DayPicker extends React.Component<DayPickerProps, any> {
   showNextMonth(): void;
   showPreviousYear(): void;
   showNextYear(): void;
-  renderMonths(): React.ReactElement<any> | null;
+  renderMonths(): React.ReactElement<any>[] | null;
 }
